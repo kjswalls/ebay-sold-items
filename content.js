ee3cecfe3cc05ddf8f2acb6f68aac8113882d5dc
Kirby Walls <kjswalls@gmail.com>
2022-03-11 11:26:40 -0800

    Initial commit

--- content.js
+++ content.js
@@ -1,4 +1,30 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+const EBAY_SOLD_LISTINGS_PARAM_KEY = 'LH_Sold';
+const EBAY_COMPLETED_LISTINGS_PARAM_KEY = 'LH_Complete';
+
+function createSoldListingsURL() {
+  const soldListingsURLTemplate = new URL(window.location.href);
+  const params = soldListingsURLTemplate.searchParams;
+  params.append(EBAY_SOLD_LISTINGS_PARAM_KEY, 1);
+  params.append(EBAY_COMPLETED_LISTINGS_PARAM_KEY, 1);
+  return soldListingsURLTemplate;
 }
+
+function createButton() {
+  const tabButtonList = document.querySelector('.srp-controls__row-cells.fake-tabs__items');
+  const firstButton = tabButtonList.querySelector('.fake-tabs__item.btn');
+  
+  const soldButton = firstButton.cloneNode(true);
+  soldButton.classList.add('soldButton');
+  
+  const buttonSpan = soldButton.querySelector('span');
+  buttonSpan.textContent = 'Sold Listings';
+
+  tabButtonList.prepend(soldButton);
+}
+
+createButton();
+const soldListingsURL = createSoldListingsURL();
+const soldButton = document.querySelector('.srp-controls__row-cells .soldButton');
+soldButton.addEventListener('click', (e) => {
+  window.location.assign(soldListingsURL);
+});
